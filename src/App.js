b086e176b962f95f016153731bfa0408bd8d7c31
madmanforces <hwangcc1019@naver.com>
2021-11-02 20:50:38 +0900

    propstype

--- src/App.js
+++ src/App.js
@@ -1,48 +1,70 @@
 import React from "react";
-
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
-    name: "Kimchi",
+    id: 1,
+    name: "진격의 거인",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "https://i.pinimg.com/564x/f1/5a/a4/f15aa478b91f2ae31b2695ff0293e57c.jpg",
+    rating: 4.2,
   },
   {
-    name: "Samgyeopsal",
+    id: 2,
+    name: "귀멸의 칼날",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+    "https://i.pinimg.com/564x/0b/4d/2d/0b4d2da488f93ad774c5f99df93f2d7b.jpg",
+    rating: 4.9
   },
   {
-    name: "Bibimbap",
+    id: 3,
+    name: "도깨비",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+    "https://i.pinimg.com/564x/e7/db/18/e7db18a2b090aaa19871c8f82f93dc6d.jpg",
+    rating: 4.8
   },
   {
-    name: "Doncasu",
+    id: 4,
+    name: "시간을 달리는 소녀",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    "https://i.pinimg.com/564x/08/45/9c/08459cfc5d5e6c4a59e55b0961c5d2f0.jpg",
+    rating: 5.5
   },
   {
-    name: "Kimbap",
+    id: 5,
+    name: "너의 이름은",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+    "https://i.pinimg.com/564x/2c/7c/8f/2c7c8f9505f9e5d005abe8c3868a5fd0.jpg",
+    rating: 4.7
   }
 ];
-
-function App() {
+function Food({ name, picture, rating }) {
   return (
     <div>
-      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
+      <h2> [인기] 영화/드라마 : {name}</h2>
+      <h4>평점 : {rating}/5.0</h4>
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+         <Food
+         key={dish.id}
+         name={dish.name}
+         picture={dish.image}
+         rating={dish.rating}
+       />
+     ))}
+   </div>
+ );
+}
 export default App;
