22fdc4c9ae78c52bef40d82c225c90390fbd0d52
madmanforces <hwangcc1019@naver.com>
2021-11-09 22:34:43 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import Button from "./Button";
-import styles from "./app.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue ((prev) => prev + 1); 
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>welcome!</h1>
-      <Button text={"continue"} />
-   </div>
- );
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
 }
 export default App;
