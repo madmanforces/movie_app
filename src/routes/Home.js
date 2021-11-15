b23d7940e4d640c68c062614369c078e7d2b99d1
madmanforces <hwangcc1019@naver.com>
2021-11-15 23:53:22 +0900

    react hoooks

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
+  const getMovies = useCallback( async() => {
     const json = await (
       await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
@@ -12,10 +12,10 @@ function Home() {
     ).json();
     setMovies(json.data.movies);
     setLoading(false);
-  };
+  });
   useEffect(() => {
     getMovies();
-  }, []);
+  }, [getMovies,]);
   return (
     <div>
       {loading ? (
@@ -25,6 +25,7 @@ function Home() {
           {movies.map((movie) => (
             <Movie
               key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
